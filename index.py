bdbbff8fbd32c12484035660250e1d7b0509f39b
NamLong <namlong135@gmail.com>
2022-08-22 16:33:12 +1000

    update csv

--- index.py
+++ index.py
@@ -1,12 +1,29 @@
-import openpyxl
+import csv
 
-# Define variable to load the dataframe
-dataframe = openpyxl.load_workbook("sample.xlsx")
+# m and n are the SLA assessment threshhold
+m = 100
+n = 95
 
-# define var to read the sheet
-dataframe1 = dataframe.active
 
-# loop to read the dataframe
-for row in range(0, dataframe1.max_row):
-  for col in dataframe1.iter_cols(1, dataframe1.max_column):
-    print(col[row].value)
+class EFN_Reputation:
+
+    def calc_res_time(self, sla_time, res_time):
+        sla_percentage = (sla_time / res_time) * 100
+        if (sla_percentage >= m):
+            return 1
+        elif (sla_percentage > n and sla_percentage < m):
+            return 0
+        elif (sla_percentage <= n):
+            return -1
+
+
+efn_instance = EFN_Reputation()
+
+with open('sample.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile)
+    next(spamreader)
+    next(spamreader)
+    for row in spamreader:
+        sla_time = float(row[2])
+        res_time = float(row[3])
+        print(efn_instance.calc_res_time(sla_time, res_time))
