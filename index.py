2fc2078f4e4b3eb62b0354c79b9c4556db9f48d7
NamLong <namlong135@gmail.com>
2022-09-09 17:52:51 +1000

    update

--- index.py
+++ index.py
@@ -9,78 +9,129 @@ below_counter = 0
 MIN_REPUTATION = 1
 MAX_REPUTATION = 5
 
-def calc_res_time(row):
+
+def calc_res_time(row, index, indexRange):
     current_reputation = int(row[0])
     sla_time = round(float(row[2]), 2)
-    for cell in range(3, len(row)):
+    # Calculate reputation of the entire table
+    if (index is None and indexRange is None):
 
-        # The difference between expected response time and measured response time in percentage
-        sla_percentage = (sla_time / round(float(row[cell]), 2)) * 100
+        for cell in range(3, len(row)):
+
+            # The difference between expected response time and measured response time in percentage
+            sla_percentage = (sla_time / round(float(row[cell]), 2)) * 100
+
+            # Above threshold
+            if (sla_percentage >= m):
+                current_reputation = process_above_threshold(current_reputation)
+
+            # Within threshold
+            elif (sla_percentage > n and sla_percentage < m):
+                current_reputation = process_within_threshold(current_reputation)
+
+            # Below threshold
+            elif (sla_percentage <= n):
+                current_reputation = process_below_threshold(current_reputation)
+
+    # Calculate reputation of a single row with a specific index
+    elif (index is not None and indexRange is None):
+        sla_percentage = (sla_time / round(float(row[index]), 2)) * 100
 
         # Above threshold
         if (sla_percentage >= m):
             current_reputation = process_above_threshold(current_reputation)
-            
-        # Within threshold    
+
+        # Within threshold
         elif (sla_percentage > n and sla_percentage < m):
             current_reputation = process_within_threshold(current_reputation)
 
-        # Below threshold    
+        # Below threshold
         elif (sla_percentage <= n):
             current_reputation = process_below_threshold(current_reputation)
-    
+
+    # Calculate reputation of a range of rows
+    elif (indexRange is not None and index is None):
+        for cell in range(indexRange[0], indexRange[1]):
+            sla_percentage = (sla_time / round(float(row[cell]), 2)) * 100
+
+            # Above threshold
+            if (sla_percentage >= m):
+                current_reputation = process_above_threshold(
+                    current_reputation)
+
+            # Within threshold
+            elif (sla_percentage > n and sla_percentage < m):
+                current_reputation = process_within_threshold(
+                    current_reputation)
+
+            # Below threshold
+            elif (sla_percentage <= n):
+                current_reputation = process_below_threshold(
+                    current_reputation)
+
     return current_reputation
+
 
 def process_above_threshold(reputation):
     # global above_counter
     global above_counter
 
-    if(reputation < MAX_REPUTATION - 1):
+    if (reputation < MAX_REPUTATION - 1):
         reputation = reputation + 1
-    elif(reputation == MAX_REPUTATION - 1 and above_counter < 4):
+    elif (reputation == MAX_REPUTATION - 1 and above_counter < 4):
         above_counter = increment_counter(above_counter)
-    elif(reputation == MAX_REPUTATION - 1 and above_counter == 4):
+    elif (reputation == MAX_REPUTATION - 1 and above_counter == 4):
         above_counter = reset_counter(above_counter)
         reputation = MAX_REPUTATION
-    
+
     return reputation
-        
+
+
 def process_within_threshold(reputation):
     global within_counter
-    if(within_counter < 4): 
+    if (within_counter < 4):
         within_counter = increment_counter(within_counter)
-    elif(within_counter == 4):
+    elif (within_counter == 4):
         within_counter = 0
         reputation = reputation - 1
-    
+
     return reputation
+
+
+def process_below_threshold(reputation):
+    global below_counter
+    if (reputation > MIN_REPUTATION):
+        reputation = reputation - 1
+    elif (reputation == MIN_REPUTATION and below_counter < 4):
+        below_counter = increment_counter(below_counter)
+    elif (reputation == MIN_REPUTATION and below_counter == 4):
+        reputation = 0
+
+    return reputation
+
 
 def reset_counter(counter):
     counter = 0
     return counter
 
+
 def increment_counter(counter):
     counter = counter + 1
     return counter
+
 
 def decrement_counter(counter):
     counter = counter - 1
     return counter
 
-def process_below_threshold(reputation):
-    global below_counter
-    if(reputation > MIN_REPUTATION):
-        reputation = reputation - 1
-    elif(reputation == MIN_REPUTATION and below_counter < 4):
-        below_counter = increment_counter(below_counter)
-    elif(reputation == MIN_REPUTATION and below_counter == 4):
-        reputation = 0
-    
-    return reputation
 
-with open('test_input.csv', 'r') as csvfile:
+with open('objective_4_input.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
     next(spamreader)
     next(spamreader)
     for row in spamreader:
-        print(calc_res_time(row))
+        print(calc_res_time(
+            row,
+            index=None,
+            indexRange=None
+        ))
