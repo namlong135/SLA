b6d9a3857bda406e45b1c35ab71bf661e2a807d5
NamLong <namlong135@gmail.com>
2022-11-11 16:13:44 +1100

    update

--- helper.py
+++ helper.py
@@ -25,7 +25,7 @@ def transpose_csv_file(input_csv_file_path, output_csv_file_path):
     csv.writer(open(output_csv_file_path, "w")).writerows(a)
 
 
-format_input_csv_file("./input/input_csv_v9.csv")
+format_input_csv_file("./input/input_csv_v8.csv")
 transpose_csv_file("./temp/raw1.csv", "./temp/transposed_data.csv")
 
 transpose_csv_file("./output/average_record.csv",
